c121776349f579a3e81717c388e7a0f42f7d116f
Andrey <man.liher66@gmail.com>
2022-11-22 05:02:17 +0300

    Добавлена папка Задача № 21

---
+++ ДЗ_Задача№21/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите координаты X,Y,Z первой точки");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты XYZ второй точки");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+double distDots = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+
+Console.WriteLine("Растояние между точек: " + distDots);
