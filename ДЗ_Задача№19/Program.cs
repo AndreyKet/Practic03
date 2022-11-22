ac27af353113d71181efce7e745a82bddcbe27b0
Andrey <man.liher66@gmail.com>
2022-11-22 06:09:37 +0300

    Добавлена папка Задача № 19

---
+++ ДЗ_Задача№19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите пятизначное число: ");
+string num = Console.ReadLine();
+
+for (int i = 0; i < num.Length / 2; i++)
+{
+    if (num[i] != num[num.Length - 1 - i])
+    {
+        Console.WriteLine("Не является полидромом!");
+    }
+    else
+    {
+        Console.WriteLine("Является полидромом!");
+    }
+}
